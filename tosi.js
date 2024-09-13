ea5fd606016f089cfc959c1aa9fbeb84d02b04b1
Logan534857345973 <Loganenemy@gmail.com>
2024-09-13 16:27:42 +0300

    first commit

---
+++ tosi.js
@@ -0,0 +1,48 @@
+//let admin;
+//let name;
+//name = "Иван";
+//admin = name;
+//alert(admin);
+
+//let x = "Я русский"
+//alert (x)
+
+//let name = "Ilya";
+//alert( `hello ${1}` ); // ? hello ilya
+//alert( `hello ${"name"}` ); //  hello name
+//alert( `hello ${name}` ); // hello
+
+//var Name = prompt("Как вас ЗВАТЬ?");
+//    document.write("Привет,"+ Name + "!")
+
+//let a = 1, b= 1;
+//let c = ++a;
+//let d = b++;
+//alert(b)
+
+//let a = 2;
+//let x = 1 + (a *= 2);
+//alert(x)
+
+//let a = +prompt("ПЕРВОЕ ЧИСЛО?????", 1);
+//let b = +prompt("ВТОРОЕ ЧИСЛО?????", 2);
+//alert(a + b); 
+
+//7>5
+//"стол" > "стул"
+//"4" > "52"
+//undefined == null
+//undefined === null
+//null == "\n0\n"
+//null == +"\n0\n"
+
+//if ("0") {
+//    alert('Привет')
+//}
+
+let Name = prompt("КАКОЕ ОФФИЦИАЛЬНОЕ НАЗВАНИЕ GANJASCRIPT?");
+if (Name == "ECMAScript") {
+    alert('АГАХАХАХАХАХ');
+} else {
+    alert('НЕА');
+}
